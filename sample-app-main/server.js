61f7d3034116ca7b6e6c501ed0b2e823cf8aea24
Mushfikur Rahman <mushfikur666@gmail.com>
2024-11-15 23:04:27 +0600

    first

---
+++ sample-app-main/server.js
@@ -0,0 +1,12 @@
+// server.js
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
